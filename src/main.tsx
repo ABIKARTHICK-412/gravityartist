8abe4f19031471f7fb0d377b3a22bb81ab5e537f
ABIKARTHICK-412 <Abikarthickg07@gmail.com>
2025-08-17 16:14:20 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,10 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { createRoot } from 'react-dom/client';
+import { HashRouter as Router } from 'react-router-dom';  // 👈 Import HashRouter
+import App from './App.tsx';
+import './index.css';
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById('root')!).render(
+  <Router>  {/* 👈 Wrap App with HashRouter */}
+    <App />
+  </Router>
+);
